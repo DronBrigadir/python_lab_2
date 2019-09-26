741130abec87233c4b38b75f703679afd6332022
Zubkov Andrey <zubkov.andrey.99@mail.ru>
2019-09-26 16:52:15 +0300

    ex. 2

--- ex_2.py
+++ ex_2.py
@@ -4,5 +4,9 @@ from librip.iterators import Unique
 
 data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
 data2 = gen_random(1, 3, 10)
+data3 = ['A', 'a', 'B', 'b', 'a', 'AB', 'aB', 'ab', 'Ba', 'Ab']
+print(*Unique(data2))
+print(*Unique(data3, ignore_case=True))
+print(*Unique(data3, ignore_case=False))
 
 # Реализация задания 2
