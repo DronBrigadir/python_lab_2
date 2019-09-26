4b8e906e28f7cb676155b2d431fd769031408f84
Zubkov Andrey <zubkov.andrey.99@mail.ru>
2019-09-26 18:51:23 +0300

    ex. 6

--- ex_6.py
+++ ex_6.py
@@ -6,13 +6,21 @@ from librip.decorators import print_result
 from librip.gens import field, gen_random
 from librip.iterators import Unique as unique
 
-path = None
+try:
+    path = sys.argv[1]
+except IndexError as e:
+    print('Enter path to json')
+    raise SystemExit
 
 # Здесь необходимо в переменную path получить
 # путь до файла, который был передан при запуске
 
-with open(path) as f:
-    data = json.load(f)
+try:
+    with open(path) as f:
+        data = json.load(f)
+except FileNotFoundError as e:
+    print('Wrong path')
+    raise SystemExit
 
 
 # Далее необходимо реализовать все функции по заданию, заменив `raise NotImplemented`
@@ -23,22 +31,23 @@ with open(path) as f:
 
 @print_result
 def f1(arg):
-    raise NotImplemented
+    return field(arg, 'job-name')
 
 
 @print_result
 def f2(arg):
-    raise NotImplemented
+    return list(filter(lambda item: True if str(item).startswith('программист') else False, arg))
 
 
 @print_result
 def f3(arg):
-    raise NotImplemented
+    return list(map(lambda item: item + ' с опытом Python', arg))
 
 
 @print_result
 def f4(arg):
-    raise NotImplemented
+    tmp = list(zip(arg, gen_random(100000, 200000, len(arg))))
+    return ['{}, зарплата {} руб.'.format(str(key), str(value)) for key, value in tmp]
 
 
 with timer():
